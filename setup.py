86adc961c0c4b7ed983edbc3a900a433b48691ff
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2018-07-30 12:19:41 -0300

    Release 0.1.2

--- setup.py
+++ setup.py
@@ -6,10 +6,9 @@ __author__ = 'Kelson da Costa Medeiros <kelsoncm@gmail.com>'
 setup(
     name='pyfwf',
     packages=['pyfwf',],
-    version='0.1.1',
-    download_url='https://github.com/kelsoncm/pyfwf/releases/tag/0.1.1',
+    version='0.1.2',
+    download_url='https://github.com/kelsoncm/pyfwf/releases/tag/0.1.2',
     description='Python library to manipulate fixed width file',
-    long_description=open('README.md', 'rt').read(),
     author='Kelson da Costa Medeiros',
     author_email='kelsoncm@gmail.com',
     url='https://github.com/kelsoncm/pyfwf',
