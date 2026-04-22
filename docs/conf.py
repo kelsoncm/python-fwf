fd2a9deb3ba1b8a8998df509ae4ceab9507a222f
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2026-04-22 14:56:46 -0300

    upd(docs): adicionar configuração para implantação de documentação no GitHub Pages

---
+++ docs/conf.py
@@ -0,0 +1,12 @@
+project = "Meu Projeto"
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.githubpages",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build"]
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
